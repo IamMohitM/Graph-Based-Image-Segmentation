f64ab2b67feed27fb272e6e51fba12a0b70d7bf6
Mohit Motwani <mohitmotwani54@gmail.com>
2020-06-08 11:44:02 +0530

    Using filesystem::path

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <opencv2/opencv.hpp>
 #include <string>
+#include <filesystem>
 #include "DisjointForest.h"
 #include "utils.h"
 #include "segmentation.h"
@@ -10,21 +11,23 @@ int main() {
     std::string color = "rgb";
     float gaussianBlur = 2.5;
     int minimumComponentSize = 100;
-    float kValue = 850;
+    float kValue = 900;
     int flag;
     if (color=="rgb"){
         flag = cv::IMREAD_COLOR;
     } else{
         flag = cv::IMREAD_GRAYSCALE;
     }
+
     std::string inputPath =  "/home/mo/CLionProjects/GraphBasedImageSegmentation/images/baseball.png";
+    std::filesystem::path path = std::filesystem::u8path(inputPath);
     std::string outputFolder = "/home/mo/CLionProjects/GraphBasedImageSegmentation/Results/";
     std::string baseFileName = getFileNameFromPath(inputPath);
     std::cout << "Filename: " << baseFileName << '\n';
 
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-    cv::Mat img = cv::imread(inputPath, flag);
+    cv::Mat img = cv::imread(path, flag);
     std::cout << img.size() << '\n';
     cv::GaussianBlur(img, img, cv::Size(3,3), gaussianBlur);
 
@@ -60,13 +63,15 @@ int main() {
     std::string outputPath = outputFolder+baseFileName+"-"+color+"-k"+
                               std::to_string(static_cast<int>(kValue))+'-'+std::to_string(gaussianBlur)+"-"
                               "min"+std::to_string(static_cast<int>(minimumComponentSize))+".jpg";
+
+    std::filesystem::path destinationPath = std::filesystem::u8path(outputPath);
     std::cout << "Image saved as: " << outputPath << '\n';
     cv::Mat segmentedImage = addColorToSegmentation(firstComponentStruct, img.rows, img.cols);
     std::cout << segmentedImage.size() << '\n';
     std::cout << segmentedImage.rows << '\n';
     std::cout << segmentedImage.cols<< '\n';
     cv::imshow("Image", segmentedImage);
-    cv::imwrite(outputPath, segmentedImage);
+    cv::imwrite(destinationPath, segmentedImage);
     cv::waitKey(0);
 
     return 0;
